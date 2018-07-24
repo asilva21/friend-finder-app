63ffc71857e38e949ab08219dee5758c9c6ed886
Anthony Silva <asilva2191@gmail.com>
2018-07-24 14:05:43 -0700

    update

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+  {
+    name: "Rihanna",
+    photo: "https://static.highsnobiety.com/wp-content/uploads/2017/06/22143628/rihanna-instagram-heartbroken-fan-01-480x320.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Megan Fox",
+    photo: "https://d.ibtimes.co.uk/en/full/1636611/megan-fox.jpg?w=736&e=c1f925bca606839ff22b848462552d72",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Iggy Azelea",
+    photo: "http://s3.amazonaws.com/hiphopdx-production/2017/11/Iggy-Azalea-826x620.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Angelina Jolie",
+    photo: "https://i.pinimg.com/236x/dc/8c/7d/dc8c7d0851c9fda3e6273012ef4da638--angelina-joile-brad-pitt-and-angelina-jolie.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Jennifer Lopez",
+    photo: "https://www.wellandgood.com/wp-content/uploads/2018/03/jennifer-lopez.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Sofia Vergara",
+    photo: "https://cmgpbpquickpulse.files.wordpress.com/2015/03/sofiavergara2003latinwe.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
